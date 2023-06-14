ec9cfc62e553a73529b8c4109f3003eafb807358
Adrian Garcia Badaracco <1755071+adriangb@users.noreply.github.com>
2023-06-14 18:11:20 -0500

    Fix array_to_utf8_json_array flag to produce large / non large StringArray outputs

--- json/python/arrow_json/__init__.py
+++ json/python/arrow_json/__init__.py
@@ -1,4 +1,19 @@
-from arrow_json._arrow_json import array_to_utf8_json_array
+from typing import Any
+
+from arrow_json._arrow_json import array_to_utf8_json_array as _array_to_utf8_json_array
 
 
-__all__ = ["array_to_utf8_json_array"]
+def array_to_utf8_json_array(array: Any, large: bool = True) -> Any:
+    """Encode a pyarrow  into a UTF-8 array of JSON data.
+
+    Args:
+        array (Any): The PyArrow array
+        large (bool, optional): wether to use a LargeString array
+            as the output or just a String array.
+            If in doubt, use `large=True`.
+            Defaults to True.
+
+    Returns
+        Any: A PyArrow array.
+    """
+    return _array_to_utf8_json_array(array, large)
--- json/src/lib.rs
+++ json/src/lib.rs
@@ -1,3 +1,4 @@
+use arrow::array::StringBuilder;
 use pyo3::prelude::*;
 use pyo3::Python;
 
@@ -7,20 +8,32 @@ use arrow::pyarrow::PyArrowConvert;
 use serde_json::{to_string, Value};
 
 #[pyfunction]
-#[pyo3(signature = (array))]
-fn array_to_utf8_json_array(py: Python, array: &PyAny) -> PyResult<PyObject> {
+#[pyo3(signature = (array, large))]
+fn array_to_utf8_json_array(py: Python, array: &PyAny, large: bool) -> PyResult<PyObject> {
     // This is super inefficient, leaving optimization as a TODO
     let array = make_array(ArrayData::from_pyarrow(array)?);
     let json = array_to_json_array(&array).unwrap();
-    let mut builder = LargeStringBuilder::new();
-    for value in json.into_iter() {
-        match value {
-            Value::Null => builder.append_null(),
-            value => builder.append_value(to_string(&value).unwrap()),
+    if large {
+        let mut builder = LargeStringBuilder::new();
+        for value in json.into_iter() {
+            match value {
+                Value::Null => builder.append_null(),
+                value => builder.append_value(to_string(&value).unwrap()),
+            }
         }
+        let json_arr = builder.finish();
+        json_arr.data().to_pyarrow(py)
+    } else {
+        let mut builder = StringBuilder::new();
+        for value in json.into_iter() {
+            match value {
+                Value::Null => builder.append_null(),
+                value => builder.append_value(to_string(&value).unwrap()),
+            }
+        }
+        let json_arr = builder.finish();
+        json_arr.data().to_pyarrow(py)
     }
-    let json_arr = builder.finish();
-    json_arr.data().to_pyarrow(py)
 }
 
 #[pymodule]
